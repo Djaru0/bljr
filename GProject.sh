cce283ee19e5717c9499794d2383084e86143b52
Djaru0 <55936588+Djaru0@users.noreply.github.com>
2019-10-02 12:19:10 +0700

    Add files via upload

--- GProject.sh
+++ GProject.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+clear
+echo "SEBENTAR"
+sleep 2
+echo "troll.sh sudah di chmod 777 belum"
+sleep 0.3
+echo "tolong di chmod dulu ya takut ada error"
+sleep 5
 read -p "masukan Nama mu :" nama
 echo "hai,selamat datang tuan" $nama
 sleep 3
@@ -18,6 +25,7 @@ echo "WAITING"
 sleep 2
 echo "installing curl"
 cd $home
+chmod +x troll.sh
 apt update && apt upgrade
 apt install curl y
 sleep 2
