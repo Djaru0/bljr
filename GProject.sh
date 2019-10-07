ed9e02c50411ccad28ac59e01180902975087528
Djaru0 <55936588+Djaru0@users.noreply.github.com>
2019-10-07 18:41:12 +0700

    Add files via upload

--- GProject.sh
+++ GProject.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 clear
-echo "SEBENTAR"
-sleep 2
-echo "troll.sh sudah di chmod 777 belum"
-sleep 0.3
-echo "tolong di chmod dulu ya takut ada error"
-sleep 5
 read -p "masukan Nama mu :" nama
 echo "hai,selamat datang tuan" $nama
 sleep 3
 clear
 echo "___________________________________"
 echo "|Selamat datang di project XDjaruX |"
-echo "|Silahkan mau install apa          |"
+echo "|Silahkan mau menjalankan apa      |"
 echo "|__________________________________|"
 echo "[1].install Alat² "
-echo "[2].RunProject"
-echo "[3].exit"
+echo "[2].Run Spam Call & Telpon"
+echo "[0].exit"
 read -p "masukan pilihan lu di sini :" pilih
 if [ $pilih = "1" ]
 then
@@ -27,7 +21,7 @@ echo "installing curl"
 cd $home
 
 apt update && apt upgrade
-apt install curl y
+apt install curl -y
 sleep 2
 reset
      echo "____________________________"
@@ -35,15 +29,23 @@ reset
      echo "|Penginstalan selesai..... |"
      echo "|__________________________|"
 clear
+figlet Recode | lolcat
+sleep 0.5
+figlet Auto | lolcat
+sleep 0.5
+figlet ERROR!! | lolcat
+sleep 4
+clear
+
 echo "___________________________________"
 echo "|Selamat datang di project XDjaruX |"
 echo "|Silahkan mau install apa          |"
 echo "|__________________________________|"
 echo "[1].install Alat² "
-echo "[2].RunProject"
-echo "[3].exit"
+echo "[2].Run Spam Call & Telpon"
+echo "[0].exit"
 read -p "masukan pilihan lu di sini :" pilih
-elif [ $pilih = "3" ]
+elif [ $pilih = "0" ]
 then
     echo "exit"
     echo "terima kasih sudah mencoba script saya"
@@ -55,5 +57,13 @@ if [ $pilih = "2" ]
 then
 cd $home
 cd masih-belajar
-sh troll.sh
+sh .troll.sh
+fi
+
+if [ $pilih = "4" ]
+then
+
+cd $home
+cd masih-belajar
+sh .b.sh
 fi
